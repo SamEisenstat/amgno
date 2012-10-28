e09f45e4487791043c63b9efd23bbb8989c0a9d8
Sam <sameisenstat@yahoo.com>
2012-10-28 03:23:47 -0400

    Improved database seeding.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,31 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-require Dir.pwd+'/db/seed-logs.rb'
+class String
+  def base_name
+    match(/([0-9a-f]*)\.png/)[1]
+  end
+end
 
-seed_logs
+logs = Set.new(Log.all.map { |log| log.url.base_name })
+count = 0
+Dir.glob('../test/*.png').each_slice(100) do |slice|
+  ActiveRecord::Base.transaction do
+    slice.each do |file|
+      base_name = file.base_name
+      unless logs.include?(base_name)
+        begin
+          transcript = IO.read(file.match(/(.*)\.png/)[1]+'.txt.fixed')
+        rescue
+          transcript = nil
+        end
+        log = Log.new :url => 'http://l.omegle.com/'+base_name+'.png',
+          :public_url => 'http://l.omegle.com/'+base_name,
+          :transcript => transcript
+        log.save
+      end
+    end
+  end
+  puts count += 1
+end
 
