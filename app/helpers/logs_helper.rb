9ca052665dfc63d6abaf4b45fdb8c76ab870b11f
Sam <sameisenstat@yahoo.com>
2012-11-11 05:57:49 -0500

    Improved search results page. The line numbers now don't count consecutive newlines, so they are accurate, the count of matches per log look better, and the total count of matched logs just says "lots" if there are too many to count.

--- app/helpers/logs_helper.rb
+++ app/helpers/logs_helper.rb
@@ -2,6 +2,7 @@ module LogsHelper
   # Creates an array of nice hashes that represent each occurrence of the
   # phrase in the text.
   def matches(text, phrase)
+    text = text.gsub(/\n+/, "\n")
     text = h text
     phrase = h phrase
     result = []
@@ -17,7 +18,7 @@ module LogsHelper
                    gsub(phrase_re, '<strong class="highlight">\0</strong>').
                    gsub("You:", '<strong class="highlight you-result">\0</strong>').
                    gsub("Stranger:", '<strong class="highlight stranger-result">\0</strong>').
-                   gsub(/\n+/, "<br>").
+                   gsub("\n", "<br>").
                    html_safe}
       
       # Don't keep going if there are too many results, to save server time.
